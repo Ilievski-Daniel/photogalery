412330f2d13db3dcf9bb1a951752d6d6b3eb6099
Ilievski-Daniel <ilievskiot@hotmail.com>
2022-04-13 15:47:02 +0200

    Create dashboard view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+<div class="b-example-divider"></div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
+  <div class="d-flex flex-column flex-shrink-0 p-3 bg-light" style="width: 280px;">
+    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+      <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
+      <span class="fs-4">Gallery</span>
+    </a>
+    <hr>
+    <ul class="nav nav-pills flex-column mb-auto">
+        <li class="nav-item">
+            <a href="#" class="nav-link active" aria-current="page">
+                <svg class="bi me-2" width="16" height="16"><use xlink:href="/home"/></svg>
+                Dashboard
+            </a>
+        </li>
+        <li>
+            <a href="/" class="nav-link link-dark">
+                <svg class="bi me-2" width="16" height="16"><use xlink:href="/"/></svg>
+                Home
+            </a>
+        </li>
+        <li>
+            <a href="/" class="nav-link link-dark">
+                <svg class="bi me-2" width="16" height="16"><use xlink:href="/"/></svg>
+                Add Image
+            </a>
+        </li>
+    </ul>
+    <hr>
     </div>
-</div>
+  </div>
 @endsection
