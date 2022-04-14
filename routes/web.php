71d888d02af0b5bf8b949ceab9bd48986480623b
Ilievski-Daniel <ilievskiot@hotmail.com>
2022-04-14 13:03:58 +0200

    Create middleware auth routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,10 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware' => 'auth'], function () {
+    // User needs to be authenticated to enter here.
+    Route::get('/users', function ()    {
+        return view('users')->with('users', User::all());
+    });
+});
